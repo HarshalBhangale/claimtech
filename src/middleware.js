7082182916f53447bc577e422caf0ea2813f91b5
Harshal Bhangale <buddyharshal2751@gmail.com>
2025-04-29 14:42:28 +0530

    feat: check deployment

--- src/middleware.js
+++ src/middleware.js
@@ -4,8 +4,8 @@ export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Skip static files and Next.js internals
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
